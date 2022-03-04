846b7f6c133f1f085bfe69660197b9b087beeb42
yifuyang <yifu.yang@ucdconnect.ie>
2022-03-04 22:16:55 +0000

    logic change

--- scenario_2_TrafficInfo/src/main/java/service/scenario_2/controllers/TrafficInfoController.java
+++ scenario_2_TrafficInfo/src/main/java/service/scenario_2/controllers/TrafficInfoController.java
@@ -1,23 +1,19 @@
 package service.scenario_2.controllers;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import service.scenario_2.access.RestrictUserAccess;
-import service.scenario_2.bean.TokenPool;
-import service.scenario_2.constant.UserLevel;
+
 
 @Controller
 public class TrafficInfoController {
-    @Autowired
-    TokenPool tokenPool;
-    @RestrictUserAccess(requiredLevel = UserLevel.admin)
+
     @RequestMapping(value="/traffic",method= RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<Boolean> requiretraffic(@RequestHeader("token") String token){
+    public ResponseEntity<Boolean> requiretraffic(){
+        System.out.println("qiuqiule");
         return new ResponseEntity<>(true, HttpStatus.ACCEPTED);
     }
 }
